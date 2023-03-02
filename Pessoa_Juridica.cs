9e31568b4c050aa9676994aed80b3efaa6b4b9a1
mayasmim <118782408+mayasmim@users.noreply.github.com>
2023-03-02 20:59:14 -0300

    Add files via upload

---
+++ Pessoa_Juridica.cs
@@ -0,0 +1,18 @@
+namespace Atividade
+{
+    class Pessoa_Juridica : Clientes
+    {
+    
+        public string cnpj {get;set;}
+        public string ie {get;set;}
+
+        public override void Pagar_Imposto(float V)
+        {
+            this.valor = V;
+            this.valor_imposto = this.valor * 20/100;
+            this.total = this.valor + this.valor_imposto;
+        }
+
+    }
+    
+}
